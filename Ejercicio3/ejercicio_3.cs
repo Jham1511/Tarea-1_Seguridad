9b4aacab08c5194f98b7840102fc97a659eb4150
Josue <josue.ham@unitec.edu>
2026-02-01 00:35:47 -0600

    Ejercicio 3 y archivos innecesarios borrados

---
+++ Ejercicio3/ejercicio_3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class ejercicio_3
+{
+    static void Main()
+    {
+        string plano = "CIFRADOVIGENERE";
+        string clave = "CLAVE";
+
+        string cifrado = "";
+        for (int i = 0; i < plano.Length; i++)
+            cifrado += (char)('A' + (plano[i] - 'A' + (clave[i % clave.Length] - 'A')) % 26);
+
+        string descifrado = "";
+        for (int i = 0; i < cifrado.Length; i++)
+            descifrado += (char)('A' + (cifrado[i] - 'A' - (clave[i % clave.Length] - 'A') + 26) % 26);
+
+        Console.WriteLine("i.   Texto plano: " + plano);
+        Console.WriteLine("ii.  Texto cifrado: " + cifrado);
+        Console.WriteLine("iii. Texto descifrado: " + descifrado);
+        Console.WriteLine("iv.  ¿Plano = Descifrado?: " + (plano == descifrado));
+    }
+}
